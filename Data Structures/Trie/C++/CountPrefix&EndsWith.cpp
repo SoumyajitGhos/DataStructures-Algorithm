1fafb2e52e27c0e91d9035e55f0e1cfbb560473e
SoumyajitGhos <soumysjitghos@gmail.com>
2024-06-05 07:46:19 +0530

    Completed the internal function of the Trie that are insert, countWordsEqualTo, countWordsStartingWith, erase

--- Data Structures/Trie/C++/CountPrefix&EndsWith.cpp
+++ Data Structures/Trie/C++/CountPrefix&EndsWith.cpp
@@ -19,11 +19,86 @@ struct Node {
         return links[ch - 'a'] != NULL;
     }
 
-    void updateEndsWithCnt() {
+    void increaseEndsWithCnt() {
         cntEndsWith++;
     }
 
-    void updatePrefixCnt() {
+    void increasePrefixCnt() {
         cntPrefix++;
     }
+
+    int endsWithCnt() {
+        return cntEndsWith;
+    }
+
+    int prefixCnt() {
+        return cntPrefix;
+    }
+
+    void reducePrefix() {
+        cntPrefix--;
+    }
+
+    void deleteEnd() {
+        cntEndsWith--;
+    }
+
+    bool anyTriePresent() {
+        bool isPresent = false;
+        for (int i = 0; i < 26; i++) {
+            isPresent = isPresent || links[i] != NULL;
+        }
+        return isPresent;
+    }
+};
+
+class Trie {
+    private:
+        Node* root;
+
+    public:
+        Trie() {
+            root = new Node();
+        }
+
+        void insert(string word) {
+            Node* node = root;
+            for (int i = 0; i <= word.length(); i++) {
+                if(!node->containsKey(word[i])) node->put(word[i]);
+                node = node->get(word[i]);
+                node->increasePrefixCnt();
+            }
+            node->increaseEndsWithCnt();
+        }
+
+        int countWordsEqualTo(string word) {
+            Node* node = root;
+            for (int i = 0; i <= word.length(); i++) {
+                if(!node->containsKey(word[i])) return 0;
+                node = node->get(word[i]);
+            }
+            return node->endsWithCnt();
+        }
+
+        int countWordsStartingWith(string word) {
+            Node* node = root;
+            for (int i = 0; i <= word.length(); i++) {
+                if(!node->containsKey(word[i])) node->put(word[i]);
+                node = node->get(word[i]);
+            }
+            return node->prefixCnt();
+        }
+
+        void erase(string word) {
+            Node* node = root;
+            for(int i = 0; i <= word.length(); i++) {
+                if(!node->containsKey(word[i])) return;
+                node = node->get(word[i]);
+                node->reducePrefix();
+            }
+            node->deleteEnd();
+            if(node->anyTriePresent()) {
+                delete node;
+            }
+        }
 };
