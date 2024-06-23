39f28ecf2b7da23e84e213dad90a1d17a29aa1d8
SoumyajitGhos <soumysjitghos@gmail.com>
2024-06-23 23:58:46 +0530

    Implemented the PercolateUp, isOverflow, HeapPush function

--- Data Structures/Heap/C++/Implementation.cpp
+++ Data Structures/Heap/C++/Implementation.cpp
@@ -7,19 +7,44 @@ using namespace std;
 class Heap {
     private:
         int MaxHeapSize  = 1024;
-        int currentHeapSize = 100;
+        int currentHeapSize = 0;
         vector<int> heap;
 
     public:
         /* Constructor */
-        Heap() : heap(currentHeapSize, -1) {}
+        Heap() : heap(currentHeapSize) {}
+
+        bool isOverflow() {
+            if(currentHeapSize >= MaxHeapSize) {
+                throw overflow_error("Heap size overflow: Attempt to increase heap size beyond maximum allowed limit");
+                return true;
+            }
+            return false;  
+        }
 
         void increaseHeapSize(int value) {
             if(currentHeapSize + value > MaxHeapSize) {
                 throw overflow_error("Heap size overflow: Attempt to increase heap size beyond maximum allowed limit");
             }
             currentHeapSize += value;
-            heap.resize(currentHeapSize, -1);
+            heap.resize(currentHeapSize);
             cout << "Heap size increased to " << currentHeapSize << endl;
+        }
+
+        /*Percolate-up algorithm*/
+        void percolateUp(int index) {
+            while (index > 0 && heap[index] > heap[(index - 1)/2]) {
+                swap(heap[index], heap[(index - 1)/2]);
+                index = (index - 1)/2; // Current pointer moves to parent;
+            }  
+        }
+
+        void push(int val) {
+            if (isOverflow()) return;
+            int currIndex = currentHeapSize;
+            /* Inserting the value in the last node or the last position in array representation */
+            heap[currIndex] = val;
+            percolateUp(currIndex);
+            currentHeapSize++;
         }
 };
